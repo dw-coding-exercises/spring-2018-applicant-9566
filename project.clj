ea3035466cea16bb392f8771132030ac7477c63c
Anonymous Applicant <anonymous@example.com>
2018-04-25 12:14:13 -0400

    Added clj-http but had difficulties and ran out of time attempting to implement.

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
                  [ring "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [compojure "1.6.0"]
+                 ;[clj-http "3.7.0"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler my-exercise.core/handler}
   :aliases {"submit" ["run" "-m" "my-exercise.submit"]})
+
+  ; I am trying to use the clj-http client to make an API request in lieu of directly making a curl request.
+  ; Due to technical issues and not being able to download, I had to comment it out for now.
--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -3,6 +3,7 @@
 (ns my-exercise.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            ;[clj-http.client :as client] ; I added the clj-http.client here in lieu of directly calling curl. It's commented due to technical issues.
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
             [my-exercise.home :as home]))
@@ -10,8 +11,11 @@
 ; This function simplifies the code for adding values
 (defn ocdid [id val]
   "Produce the ocd id for a particular val"
-  (concat (concat (concat "/" id) ":") (clojure.string/lower-case (clojure.string/replace val #" " "_"))))
+  (if (clojure.string/blank? val) ; If the user leaves the area blank, do not include it in the search
+  (str nil)
+  (concat (concat (concat "/" id) ":") (clojure.string/lower-case (clojure.string/replace val #" " "_")))))
 ; Notice that the id does not have to be lowercase here, since this is not based on user input.
+
 
 (defroutes app
   (GET "/" [] home/page)
@@ -23,7 +27,24 @@
             state (get (:params req) :state)
             zip (get (:params req) :zip)]
 
-        (concat (concat (concat (concat (concat (concat "ocd-division/country:us" (ocdid "street" street)) (ocdid "street-2" street-2))) (ocdid "place" city)) (ocdid "state" state)) (ocdid "zip" zip))
+        ; Define the state and place.
+        (def state_ocdid (concat (concat "ocd-division/country:us" (ocdid "state" state)))) ; Ooooo ocdid Fxn Call!
+        (def place_ocdid (concat state_ocdid (ocdid "place" city))) ; Ooooo another ocdid Fxn call! :-)
+
+        ; I know you didn't ask for this, but I was having fun! I've never dealt with Clojure before!
+        (def full_ocdid (concat (concat (concat (concat (concat (concat "ocd-division/country:us" (ocdid "street" street)) (ocdid "street-2" street-2))) (ocdid "place" city)) (ocdid "state" state)) (ocdid "zip" zip)))
+
+        ;If given the chance to do a next version, I would refactor to clean up the multiple parentheses with the concat function.
+        (def api (concat (concat (concat (concat "'https://api.turbovote.org/elections/upcoming?district-divisions=" state_ocdid) ",") place_ocdid) "'"))
+
+        ; I did not get to test this due to technical issues. This line is commented just to make sure it at least compiles.
+        ; (client/get api)
+
+        ;TESTING, although rudimentary!
+        ; (println state_ocdid)
+        ; (println place_ocdid)
+        ; (println full_ocdid)
+        ; (println api) ; I have to get used to putting the parentheses outside! :-)
 
         ))
 
